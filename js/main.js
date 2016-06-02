97f281d2cc98c47cc795fe31f79301fba76e8146
SandraBollo <sandra.bollo.b@gmail.com>
2016-06-02 13:10:13 -0500

    portafolio

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#fondo1').click(function(){
+        $('#one').slideToggle( { direction: "left" }, 1000);
+    });
+});
+// $(document).ready(function(){
+//     $("#flip").click(function(){
+//         $("#panel").slideToggle({ direction: "top" }, 1000);
+
+//     });
+// });
+
